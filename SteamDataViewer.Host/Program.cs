4b03cda5e63d50a1d1da60dffb6fae28b758821d
Evan ROUSSIN <evan.roussin@gmail.com>
2024-03-31 23:27:27 +0200

    Cleanup of WPF because I hate it

--- SteamDataViewer.Host/Program.cs
+++ SteamDataViewer.Host/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SteamDataViewer.Data.Configuration;
-using SteamDataViewer.UI;
 
 namespace SteamDataViewer.Host;
 
@@ -8,26 +7,15 @@ internal class Program
 {
     private static IServiceProvider serviceProvider;
     
-    [STAThread]
     private static void Main(string[] args)
     {
         ConfigureServices();
-        RunApplication();
-    }
-
-    private static void RunApplication()
-    {
-        var app = new SteamDataViewerApp(serviceProvider);
-        app.Run();
     }
 
     private static void ConfigureServices()
     {
         var services = new ServiceCollection();
-
-        services.AddAppsService()
-            .AddSingleton<AppsViewerWindow>();
-        
+        services.AddAppsService();
         serviceProvider = services.BuildServiceProvider();
     }
 }
--- SteamDataViewer.UI/DTO/GameViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-using SteamDataViewer.Data.Apps.Models;
-
-namespace SteamDataViewer.UI.DTO;
-
-public class GameViewModel(string AppId, string Name, bool IsInstalled)
-{
-    public static GameViewModel From(Game game) => new(game.AppId, game.Name, game.IsInstalled);
-};
